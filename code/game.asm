0266c4107bbd9b4f1ec6ad519ccfeb65ad662bf5
XSTNX <olhaberrynicolas@gmail.com>
2020-12-19 04:15:17 +0100

    Save video mode at start to restore it before quit.

--- code/game.asm
+++ code/game.asm
@@ -1,16 +1,31 @@
-codeData group code, data
-    assume cs:codeData, ds:codeData
+include bios.inc
+include dos.inc
+
+allSegments group code, data
+    assume cs:allSegments, ds:allSegments
 
 code segment public
 	org 100h
 
 main proc
-	int 20h
+	; Save previous video mode.
+	mov ah,FUNC_BIOS_VIDEO_GET_VIDEO_MODE
+	int INT_BIOS_VIDEO
+	mov [PrevVideoMode],al
+
+	; Restore previous video mode.
+	mov al,[PrevVideoMode]
+	mov ah,FUNC_BIOS_VIDEO_SET_VIDEO_MODE
+	int INT_BIOS_VIDEO
+
+	; Quit.
+	int INT_DOS_COM_TERMINATION
 main endp
 
 code ends
 
 data segment public
+	PrevVideoMode		db ?
 data ends
 
 	end main
