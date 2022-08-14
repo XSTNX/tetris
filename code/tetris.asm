38dc520ce82872f4897f576d1135e4abe27903bf
XSTNX <olhaberrynicolas@gmail.com>
2022-08-14 23:31:32 +0200

    Render one block at 0,0.

--- code/tetris.asm
+++ code/tetris.asm
@@ -7,6 +7,8 @@ include code\keyboard.inc
 include code\render.inc
 include code\timer.inc
 
+NEXT_LINE_OFFSET        equ (BIOS_VIDEO_MODE_320_200_4_BYTES_P_LINE - 2)
+
 code segment readonly public
 
 ; ------------;
@@ -26,6 +28,21 @@ tetrisUpdate proc
 tetrisUpdate endp
 
 tetrisRender proc
+    mov ax,0ffffh
+    xor di,di
+repeat 3
+    stosw
+    add di,NEXT_LINE_OFFSET
+endm
+    stosw
+
+    mov di,BIOS_VIDEO_MODE_320_200_4_BANK1_OFFSET
+repeat 3
+    stosw
+    add di,NEXT_LINE_OFFSET
+endm
+    stosw
+
     ret
 tetrisRender endp
 
