ff81d798b99e8169303394e28c4efb36892dc608
XSTNX <olhaberrynicolas@gmail.com>
2022-08-15 00:48:39 +0200

    Started drawing the limits of the board.

--- code/tetris.asm
+++ code/tetris.asm
@@ -7,7 +7,12 @@ include code\keyboard.inc
 include code\render.inc
 include code\timer.inc
 
-NEXT_LINE_OFFSET        equ (BIOS_VIDEO_MODE_320_200_4_BYTES_P_LINE - 2)
+TETRIS_BLOCK_SIZE                   equ 8
+TETRIS_BOARD_COLS                   equ 10
+TETRIS_BOARD_ROWS                   equ 20
+TETRIS_BOARD_START_POS_X            equ BIOS_VIDEO_MODE_320_200_4_HALF_WIDTH - ((TETRIS_BOARD_COLS / 2) * TETRIS_BLOCK_SIZE)
+TETRIS_BOARD_START_POS_Y            equ BIOS_VIDEO_MODE_320_200_4_HALF_HEIGHT - ((TETRIS_BOARD_ROWS / 2) * TETRIS_BLOCK_SIZE)
+TETRIS_RENDER_NEXT_LINE_OFFSET      equ (BIOS_VIDEO_MODE_320_200_4_BYTES_P_LINE - 2)
 
 code segment readonly public
 
@@ -20,6 +25,20 @@ tetrisInit proc
 tetrisInit endp
 
 tetrisInitRender proc
+    ; Top limit.
+    mov cx,TETRIS_BOARD_START_POS_X
+    push cx
+    mov di,TETRIS_BOARD_START_POS_X + (TETRIS_BOARD_COLS * TETRIS_BLOCK_SIZE)
+    push di
+    mov dl,TETRIS_BOARD_START_POS_Y - 1
+    call renderHorizLine320x200x4
+    ; Bottom limit.
+    pop di
+    pop cx
+    mov dl,TETRIS_BOARD_START_POS_Y + (TETRIS_BOARD_ROWS * TETRIS_BLOCK_SIZE)
+    call renderHorizLine320x200x4
+    ; Left limit.
+    ; Right limit.
     ret
 tetrisInitRender endp
 
@@ -28,27 +47,46 @@ tetrisUpdate proc
 tetrisUpdate endp
 
 tetrisRender proc
-    mov ax,0ffffh
-    xor di,di
-repeat 3
-    stosw
-    add di,NEXT_LINE_OFFSET
-endm
-    stosw
-
-    mov di,BIOS_VIDEO_MODE_320_200_4_BANK1_OFFSET
-repeat 3
-    stosw
-    add di,NEXT_LINE_OFFSET
-endm
-    stosw
-
+    mov ax,0aaaah
+    xor dx,dx
+    call tetrisRenderBlock
     ret
 tetrisRender endp
 
 ; -------------;
 ; Code private ;
 ; -------------;
+
+; Input: ax (color for the whole block, 8 pixels corresponding to 16bits), dl (unsigned col), dh (unsigned row).
+; Clobber: di.
+tetrisRenderBlock proc private
+if ASSERT_ENABLED
+    cmp dl,TETRIS_BOARD_COLS
+    jb @f
+    ASSERT
+@@:
+    cmp dh,TETRIS_BOARD_ROWS
+    jb @f
+    ASSERT
+@@:
+endif
+
+    xor di,di
+repeat 3
+    stosw
+    add di,TETRIS_RENDER_NEXT_LINE_OFFSET
+endm
+    stosw
+
+    mov di,BIOS_VIDEO_MODE_320_200_4_BANK1_OFFSET
+repeat 3
+    stosw
+    add di,TETRIS_RENDER_NEXT_LINE_OFFSET
+endm
+    stosw
+
+    ret
+tetrisRenderBlock endp
 
 code ends
 
